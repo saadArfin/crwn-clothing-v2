cb3b362b4cec79bbfe7dd0303f54791e77651928
saadArfin <saad.arfin.ug21@nsut.ac.in>
2023-07-12 05:04:17 +0530

    Used Reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useReducer } from 'react';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 import {
   onAuthStateChangedListener,
@@ -19,6 +20,8 @@ const INITIAL_STATE = {
 };
 
 const userReducer = (state, action) => {
+  console.log("action");
+  console.log(action);
   const { type, payload } = action;
 
   switch (type) {
@@ -31,9 +34,14 @@ const userReducer = (state, action) => {
 
 export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  console.log(currentUser);
 
   const setCurrentUser = (user) =>
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, currentUser: user });
+    dispatch(
+      createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)
+    );
+      
+      
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
