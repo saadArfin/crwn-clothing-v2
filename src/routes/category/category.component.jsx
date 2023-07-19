5ff54dea0e3e76017bad5c6f0d6da96e1e4cc436
saadArfin <saad.arfin.ug21@nsut.ac.in>
2023-07-19 11:20:09 +0530

    redux thunk added

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -4,13 +4,15 @@ import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 
 import { CategoryContainer, Title } from './category.styles';
+import Spinner from '../../components/spinner/spinner.component';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -20,12 +22,14 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
+      {isLoading ? (<Spinner/>) : 
       <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </CategoryContainer>
+      </CategoryContainer>}
+      
     </Fragment>
   );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,31 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 // import logger from 'redux-logger';
-
+import {persistStore, persistReducer} from 'redux-persist';
+import  storage from 'redux-persist/lib/storage';
+import { loggerMiddleware } from './middleware/logger';
 import { rootReducer } from './root-reducer';
+import thunk from 'redux-thunk';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
 
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
 
-  next(action);
+}
 
-  console.log('next state: ', store.getState());
-};
+//const middleWare = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);//We can use this middleware also.
 
-const middleWares = [loggerMiddleware];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const middleWares = [loggerMiddleware, thunk];
+
+const composeEnhancer = (
+  process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
