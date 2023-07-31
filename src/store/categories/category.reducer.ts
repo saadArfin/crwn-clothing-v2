fc395c37444fddf27df06687572fcff203c8b7ef
saadArfin <saad.arfin.ug21@nsut.ac.in>
2023-07-31 12:42:54 +0530

    done

---
+++ src/store/categories/category.reducer.ts
@@ -0,0 +1,48 @@
+import { Category } from './category.types';
+import { FetchCategoriesStart, fetchCategoriesSuccess, fetchCategoriesFailed, fetchCategoriesStart } from './category.action';
+import {AnyAction} from 'redux';
+
+export type CategoriesState = {
+  readonly categories: Category[];
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+}
+
+export const CATEGORIES_INITIAL_STATE : CategoriesState = {
+  categories: [],
+  isLoading: false,
+  error: null,
+};
+
+// export const categoriesReducer = (
+//   state = CATEGORIES_INITIAL_STATE,
+//   action = {} as CategoryAction
+// ): CategoriesState => {
+//   // const { type, payload } = action;
+
+//   switch (action.type) {
+//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+//       return { ...state, isLoading: true };
+//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+//       return {...state, categories: action.payload, isLoading: false}
+//     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+//       return {...state, error: action.payload, isLoading: false}  
+//     default:
+//       return state;
+//   }
+// };
+export const categoriesReducer = (
+  state = CATEGORIES_INITIAL_STATE,
+  action: AnyAction 
+): CategoriesState =>{
+  if(fetchCategoriesStart.match(action)){
+    return {...state, isLoading: true};
+  }
+  if(fetchCategoriesSuccess.match(action)){
+    return {...state, categories: action.payload, isLoading: false};
+  }
+  if(fetchCategoriesFailed.match(action)){
+    return {...state, error: action.payload, isLoading: false}
+  }
+  return state;
+};
