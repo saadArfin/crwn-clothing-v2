9f7bcf4a0597491df218c25dd16be508b2f81efd
saadArfin <saad.arfin.ug21@nsut.ac.in>
2023-07-31 17:30:14 +0530

    done

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -4,6 +4,7 @@ import {
   selectCartCount,
   selectIsCartOpen,
 } from '../../store/cart/cart.selector';
+
 import { setIsCartOpen } from '../../store/cart/cart.action';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
